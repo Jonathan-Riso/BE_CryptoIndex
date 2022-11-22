6589b339db86037178327f5df1d68ee434f37ed7
Jonathan-Riso <riso.jona10@gmail.com>
2022-11-22 11:39:07 -0500

    Updated api

--- src/app.py
+++ src/app.py
@@ -1,22 +1,37 @@
 from sre_constants import SUCCESS
-from flask import Flask
+from flask import Flask, Response, abort
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson.json_util import dumps
-from src.helpers.json_helper import convert_to_json
+from helpers.json_helper import convert_to_json
 import json
 
 app = Flask(__name__)
 
 # Connects to default localhost
 client = MongoClient()
-heartbeat_collection = client.cryptoIndex.heartbeat
+scores = client.cryptoIndex.scores
 
 # https://flask.palletsprojects.com/en/2.2.x/quickstart/
 # for basics
 
-@app.route("/heartbeat")
-def heartbeat():
-    result = heartbeat_collection.insert_one({"status": "Alive"})
-    document = heartbeat_collection.find_one({"_id": ObjectId(result.inserted_id)})
-    return convert_to_json(document)
+# @app.route("/heartbeat")
+# def heartbeat():
+#     result = heartbeat_collection.insert_one({"status": "Alive"})
+#     document = heartbeat_collection.find_one({"_id": ObjectId(result.inserted_id)})
+#     return convert_to_json(document)
+
+@app.route("/results/<index>")
+def results(index):
+    print(index)
+    result = scores.find_one({"index": index.lower()})
+    if not result:
+        abort(404)
+    print(result)
+    # if result.time < 2 hours current
+    # Post new results
+    return Response(f"{convert_to_json(result)}", status=200, mimetype='application/json')
+
+# GET Tweets
+# GET News_Reports
+# POST results
--- src/helpers/json_helper.py
+++ src/helpers/json_helper.py
@@ -1,7 +1,8 @@
+import json
 
 def convert_to_json(data):
-    id = data.get("id", None)
+    id = data.get("_id", None)
     if id is None:
         raise Exception("Missing ID")
     data.update({"_id": str(id)})
-    return data
+    return json.dumps(data)
